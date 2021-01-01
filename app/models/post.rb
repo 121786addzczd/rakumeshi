e0548c1c2723258c4910861d8da88dd63f2b5a84
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-01-01 19:32:11 +0900

    投稿コメント機能実装
    非同期通信

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
 
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
 
+  has_many :comments, dependent: :destroy
+
   def liked_by(user)
     # user_idとpost_idが一致するlikeを検索する
     Like.find_by(user_id: user.id, post_id: id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   has_many :likes, dependent: :destroy
 
+  has_many :comments, dependent: :destroy
+
   has_one_attached :image
 
   # ====================自分がフォローしているユーザーとの関連 ===================================
