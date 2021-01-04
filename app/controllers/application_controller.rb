5dbac8daf0fa6af0ee524e7b6ccbfaf1584a505c
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-01-04 22:19:25 +0900

    自己紹介文のカラムを追加
    rails g migration AddSelfIntroductionToUser self_introduction:text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :image, :self_introduction])
   end
 
   # ログイン後マイページにリダイレクト
